a755f27ceea16f0ed781684d2b889d9b855e652d
Jakub Pavlík jn <severus@post.cz>
2015-06-26 19:14:51 +0200

    CLI: use thor

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'i18n'
 gem 'log4r'
 
+gem 'thor'
+
 group :development do
   gem 'rspec'
   gem 'guard-rspec', require: false
--- bin/calendariumrom.rb
+++ bin/calendariumrom.rb
@@ -1,17 +1,28 @@
 #!/bin/env ruby
 
+require 'thor'
 require 'calendarium-romanum'
 require 'log4r'
 
 # load specified calendar files, print errors
 
-include CalendariumRomanum
+module CalendariumRomanum
+  class CLI < Thor
 
-logger = Log4r::Logger['CalendariumRomanum::SanctoraleLoader']
-logger.outputters << Log4r::StderrOutputter.new('stderr')
+    desc 'errors FILE1, ...', 'finds errors in sanctorale data files'
+    def errors(*files)
+      logger = Log4r::Logger['CalendariumRomanum::SanctoraleLoader']
+      logger.outputters << Log4r::StderrOutputter.new('stderr')
 
-ARGV.each do |path|
-  s = Sanctorale.new
-  loader = SanctoraleLoader.new
-  loader.load_from_file s, path
+      files.each do |path|
+        s = Sanctorale.new
+        loader = SanctoraleLoader.new
+        loader.load_from_file s, path
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  CalendariumRomanum::CLI.start ARGV
 end
