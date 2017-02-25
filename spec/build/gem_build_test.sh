3d990796578cc04a4c2fb3ab493b07aa996f8944
Jakub Pavlík <severus@post.cz>
2017-02-25 23:42:26 +0100

    automate gem build test
    the part I hate the most about releasing new gem releases

---
+++ spec/build/gem_build_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e # fail on first error
+
+echo 'Running gem build test'
+
+## build
+gem build calendarium-romanum.gemspec
+
+## install
+GEMSET=gem_build_test_gemset
+
+# determine gem file name
+VERSION=`ruby -Ilib -rcalendarium-romanum/version -e 'puts CalendariumRomanum::VERSION'`
+GEM=calendarium-romanum-$VERSION.gem
+
+rvm gemset create $GEMSET
+rvm @$GEMSET do gem install $GEM
+
+## test loading in a program
+rvm @$GEMSET do ruby -rcalendarium-romanum -e 'p CalendariumRomanum::Ranks::FERIAL'
+echo $?
+
+## clean
+rvm gemset delete --force $GEMSET
+
+echo 'Gem build test finished successfully'
