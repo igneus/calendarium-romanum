05ccad2eff8ace493446ce0d61c4107502d40014
Jakub Pavlík <severus@post.cz>
2017-02-25 19:20:58 +0100

    don't overwrite I18n load path, add to it
    + don't touch other I18n configuration. We are a library, not an
    application. Libraries shouldn't modify the environment unless really
    necessary.

--- lib/calendarium-romanum/i18n_setup.rb
+++ lib/calendarium-romanum/i18n_setup.rb
@@ -1,4 +1,3 @@
 require 'i18n'
 
-I18n.config.load_path = Dir[File.expand_path('../../config/locales/*.yml', File.dirname(__FILE__))]
-I18n.config.enforce_available_locales = true
+I18n.config.load_path += Dir[File.expand_path('../../config/locales/*.yml', File.dirname(__FILE__))]
