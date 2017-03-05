0ec3797b75c071a20d0cc330ab71712f8237d867
Jakub Pavlík <severus@post.cz>
2017-03-05 22:01:28 +0100

    Rank descriptions can be localized
    + Rank never more inherits from Struct

--- lib/calendarium-romanum/enums.rb
+++ lib/calendarium-romanum/enums.rb
@@ -17,21 +17,21 @@ module CalendariumRomanum
     # Values are at the same time references to sections
     # of the Table of Liturgical Days.
     # The lower value, the higher rank.
-    TRIDUUM           = Rank.new 1.1, I18n.t('rank.1_1')
-    PRIMARY           = Rank.new 1.2, I18n.t('rank.1_2') # description may not be exact
-    SOLEMNITY_GENERAL = Rank.new 1.3, I18n.t('rank.1_3'), I18n.t('rank.short.solemnity') # description may not be exact
-    SOLEMNITY_PROPER  = Rank.new 1.4, I18n.t('rank.1_4'), I18n.t('rank.short.solemnity')
+    TRIDUUM           = Rank.new 1.1, 'rank.1_1'
+    PRIMARY           = Rank.new 1.2, 'rank.1_2' # description may not be exact
+    SOLEMNITY_GENERAL = Rank.new 1.3, 'rank.1_3', 'rank.short.solemnity' # description may not be exact
+    SOLEMNITY_PROPER  = Rank.new 1.4, 'rank.1_4', 'rank.short.solemnity'
 
-    FEAST_LORD_GENERAL  = Rank.new 2.5, I18n.t('rank.2_5'), I18n.t('rank.short.feast')
-    SUNDAY_UNPRIVILEGED = Rank.new 2.6, I18n.t('rank.2_6'), I18n.t('rank.short.sunday')
-    FEAST_GENERAL       = Rank.new 2.7, I18n.t('rank.2_7'), I18n.t('rank.short.feast')
-    FEAST_PROPER        = Rank.new 2.8, I18n.t('rank.2_8'), I18n.t('rank.short.feast')
-    FERIAL_PRIVILEGED   = Rank.new 2.9, I18n.t('rank.2_9'), I18n.t('rank.short.ferial')
+    FEAST_LORD_GENERAL  = Rank.new 2.5, 'rank.2_5', 'rank.short.feast'
+    SUNDAY_UNPRIVILEGED = Rank.new 2.6, 'rank.2_6', 'rank.short.sunday'
+    FEAST_GENERAL       = Rank.new 2.7, 'rank.2_7', 'rank.short.feast'
+    FEAST_PROPER        = Rank.new 2.8, 'rank.2_8', 'rank.short.feast'
+    FERIAL_PRIVILEGED   = Rank.new 2.9, 'rank.2_9', 'rank.short.ferial'
 
-    MEMORIAL_GENERAL  = Rank.new 3.10, I18n.t('rank.3_10'), I18n.t('rank.short.memorial')
-    MEMORIAL_PROPER   = Rank.new 3.11, I18n.t('rank.3_11'), I18n.t('rank.short.memorial')
-    MEMORIAL_OPTIONAL = Rank.new 3.12, I18n.t('rank.3_12'), I18n.t('rank.short.memorial_opt')
-    FERIAL            = Rank.new 3.13, I18n.t('rank.3_13'), I18n.t('rank.short.ferial')
+    MEMORIAL_GENERAL  = Rank.new 3.10, 'rank.3_10', 'rank.short.memorial'
+    MEMORIAL_PROPER   = Rank.new 3.11, 'rank.3_11', 'rank.short.memorial'
+    MEMORIAL_OPTIONAL = Rank.new 3.12, 'rank.3_12', 'rank.short.memorial_opt'
+    FERIAL            = Rank.new 3.13, 'rank.3_13', 'rank.short.ferial'
 
     def self.[](priority)
       Rank[priority]
--- lib/calendarium-romanum/rank.rb
+++ lib/calendarium-romanum/rank.rb
@@ -1,21 +1,33 @@
 module CalendariumRomanum
-  class Rank < Struct.new(:priority, :desc, :short_desc)
+  class Rank
     include Comparable
 
     @@instances = {}
 
-    def initialize(*args)
-      super(*args)
+    def initialize(priority=nil, desc=nil, short_desc=nil)
+      @priority = priority
+      @desc = desc
+      @short_desc = short_desc
 
       @@instances[self.priority] = self
+    end
+
+    attr_reader :priority
+    alias_method :to_f, :priority
+
+    def desc
+      @desc && I18n.t(@desc)
+    end
+
+    alias_method :to_s, :desc
+
+    def short_desc
+      @short_desc && I18n.t(@short_desc)
     end
 
     def <=>(b)
       b.priority <=> self.priority
     end
-
-    alias_method :to_f, :priority
-    alias_method :to_s, :desc
 
     def self.[](priority)
       @@instances[priority]
