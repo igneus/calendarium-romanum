60aea9a9a2580e5e25298c736cb7d04e22efafea
Jakub Pavlík <severus@post.cz>
2017-07-19 20:19:22 +0200

    fix broken 'cmp' subcommand

--- lib/calendarium-romanum/cli.rb
+++ lib/calendarium-romanum/cli.rb
@@ -15,16 +15,18 @@ module CalendariumRomanum
     end
 
     desc 'cmp FILE1, FILE2', 'detect differences in rank and colour of corresponding celebrations'
-    def rccompare(a, b)
+    def cmp(a, b)
       loader = SanctoraleLoader.new
       sanctorales = []
 
       [a, b].each do |source|
         s = Sanctorale.new
-        loader.load_from_file s, source
+        loader.load_from_file source, s
         sanctorales << s
       end
 
+      # a leap year must be chosen in order to iterate over
+      # all possible days of a Sanctorale
       Year.new(1990).each_day do |d|
         celebs = sanctorales.collect {|s| s.get d.month, d.day }
         if celebs.find {|cc| cc.nil? }
