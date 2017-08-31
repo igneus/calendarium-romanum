20714f8b4bc38f082b22af7eacc2f75bd62a079e
Jakub Pavlík <severus@post.cz>
2017-08-31 23:37:46 +0200

    PerpetualCalendar

---
+++ lib/calendarium-romanum/perpetual_calendar.rb
@@ -0,0 +1,49 @@
+module CalendariumRomanum
+  # When you want to query a calendar without caring about
+  # civil and liturgical years and Calendar instances
+  class PerpetualCalendar
+    def initialize(sanctorale: nil, temporale_factory: nil, temporale_options: nil, cache: {})
+      if temporale_factory && temporale_options
+        raise ArgumentError.new('Specify either temporale_factory or temporale_options, not both')
+      end
+
+      @sanctorale = sanctorale
+      @temporale_factory = temporale_factory || build_temporale_factory(temporale_options)
+
+      @cache = cache
+    end
+
+    # returns a resolved Day
+    def day(*args)
+      calendar_for(*args).day(*args)
+    end
+
+    # returns a Calendar instance for the liturgical year containing
+    # the specified day
+    def calendar_for(*args)
+      date = Calendar.mk_date(*args)
+      year = Temporale.liturgical_year date
+      calendar_instance year
+    end
+
+    # returns a Calendar instance for the specified liturgical year
+    def calendar_for_year(year)
+      calendar_instance year
+    end
+
+    private
+
+    def build_temporale_factory(temporale_options)
+      temporale_options ||= {}
+      lambda {|year| Temporale.new(year, **temporale_options) }
+    end
+
+    def calendar_instance(year)
+      if @cache.has_key? year
+        @cache[year]
+      else
+        @cache[year] = Calendar.new(year, @sanctorale, @temporale_factory.call(year))
+      end
+    end
+  end
+end
---
+++ spec/perpetual_calendar_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe CR::PerpetualCalendar do
+  let(:pcal) { described_class.new }
+
+  let(:date) { Date.new(2000, 1, 1) }
+  let(:year) { 2000 }
+
+  let(:sanctorale) { CR::Data::GENERAL_ROMAN_ENGLISH.load }
+
+  describe '.new' do
+    describe 'with sanctorale' do
+      it 'uses the sanctorale' do
+        pc = described_class.new(sanctorale: sanctorale)
+
+        calendar = pc.calendar_for_year(year)
+        expect(calendar.sanctorale).to be sanctorale
+      end
+    end
+
+    describe 'with temporale options' do
+      it 'applies the options' do
+        pc = described_class.new(temporale_options: {transfer_to_sunday: [:epiphany]})
+
+        y = 2016
+        calendar = pc.calendar_for_year(y)
+        epiphany_date = CR::Temporale::Dates.epiphany(y, sunday: true)
+        expect(epiphany_date).not_to eq CR::Temporale::Dates.epiphany(y) # make sure
+
+        expect(calendar.day(epiphany_date).celebrations[0].title).to eq 'The Epiphany of the Lord'
+      end
+    end
+
+    describe 'with temporale factory' do
+      it 'uses the factory' do
+        temporale_subcls = Class.new(CR::Temporale)
+        factory = lambda {|year| temporale_subcls.new year }
+
+        pc = described_class.new(temporale_factory: factory)
+        calendar = pc.calendar_for_year(year)
+        expect(calendar.temporale).to be_a temporale_subcls
+      end
+    end
+
+    describe 'with both temporale factory and options' do
+      it 'fails' do
+        expect do
+          described_class.new(
+            temporale_options: {transfer_to_sunday: [:epiphany]},
+            temporale_factory: lambda {|year| temporale_subcls.new year }
+          )
+        end.to raise_exception ArgumentError
+      end
+    end
+
+    describe 'with cache' do
+      it 'uses the supplied object as Calendar instance cache' do
+        cache = {}
+        pc = described_class.new(cache: cache)
+        calendar = pc.calendar_for_year(year)
+        expect(cache[2000]).to be calendar
+      end
+    end
+  end
+
+  describe '#day' do
+    it 'returns a Day' do
+      expect(pcal.day(Date.today)).to be_a CR::Day
+    end
+  end
+
+  describe '#calendar_for' do
+    it 'returns a Calendar' do
+      expect(pcal.calendar_for(date)).to be_a CR::Calendar
+    end
+  end
+
+  describe '#calendar_for_year' do
+    it 'returns a Calendar' do
+      expect(pcal.calendar_for_year(year)).to be_a CR::Calendar
+    end
+  end
+
+  describe 'caching' do
+    it 'caches calendar instances' do
+      expect(CR::Calendar).to receive(:new)
+
+      2.times { pcal.calendar_for_year(year) }
+    end
+  end
+end
