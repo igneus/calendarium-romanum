2307812dd8fc20e9c08d49cb19bac99dfd146b2d
Jakub Pavlík <severus@post.cz>
2017-10-07 18:33:34 +0200

    simplify

--- lib/calendarium-romanum/cli.rb
+++ lib/calendarium-romanum/cli.rb
@@ -63,18 +63,14 @@ module CalendariumRomanum
     desc 'cmp FILE1, FILE2', 'detect differences in rank and colour of corresponding celebrations'
     def cmp(a, b)
       loader = SanctoraleLoader.new
-      sanctorales = []
-
-      [a, b].each do |source|
-        s = Sanctorale.new
-        loader.load_from_file source, s
-        sanctorales << s
+      sanctorales = [a, b].collect do |source|
+        loader.load_from_file source
       end
 
       # a leap year must be chosen in order to iterate over
       # all possible days of a Sanctorale
       Year.new(1990).each_day do |d|
-        celebs = sanctorales.collect {|s| s.get d.month, d.day }
+        celebs = sanctorales.collect {|s| s.get d }
         if celebs.find {|cc| cc.nil? }
           next
         end
