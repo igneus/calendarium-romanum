9f9ff0af3c55dcda5fb87cb88076a875dae61d23
Jakub Pavlik <severus@post.cz>
2020-06-28 16:33:27 +0200

    version bump -> 0.7.1

--- lib/calendarium-romanum/version.rb
+++ lib/calendarium-romanum/version.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 module CalendariumRomanum
-  VERSION = '0.7.0'.freeze
+  VERSION = '0.7.1'.freeze
 
-  RELEASE_DATE = Date.new(2020, 6, 21).freeze
+  RELEASE_DATE = Date.new(2020, 6, 28).freeze
 end
